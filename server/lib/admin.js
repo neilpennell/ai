059cd4e310b7b9f7e750e8f13787cb6fe2d704e0
Neil Pennell <neil.pennell@gmail.com>
2015-01-10 10:41:00 -0500

    remove keys and update

--- server/lib/admin.js
+++ server/lib/admin.js
@@ -3,4 +3,3 @@
  */
 
 //process.env.MAIL_URL = 'smtp://action-item-editor%40sandbox4d7eb87468294aa6a70bd510aa03671f.mailgun.org:4d7eb87@smtp.mailgun.org:587';
-process.env.MAIL_URL   = 'smtp://action.item.editor%40horsepackage.com:8b340fc0904d2ca71e34cb00ad3d4f31@smtp.mailgun.org:587';
